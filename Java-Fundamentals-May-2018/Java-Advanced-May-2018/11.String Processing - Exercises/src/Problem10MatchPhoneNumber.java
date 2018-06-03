e88d209df6325e1f45ffe797782517fd945034d9
DimovDimo <d.dimov1996@gmail.com>
2018-06-03 19:48:38 +0300

    added Problem10MatchPhoneNumber.java in 11.String Processing - Exercises

---
+++ Java-Fundamentals-May-2018/Java-Advanced-May-2018/11.String Processing - Exercises/src/Problem10MatchPhoneNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Problem10MatchPhoneNumber {
+    public static void main(String[] args) {
+        String regex = "^\\+359([ -])2\\1\\d{3}\\1\\d{4}$";
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        while (!line.equals("end")){
+            if (Pattern.matches(regex, line)){
+                System.out.println(line);
+            }
+
+            line = scanner.nextLine();
+        }
+    }
+}
