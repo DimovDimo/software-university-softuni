af8bf581c76b4de1312a48670b47e3253e25b3e9
DimovDimo <d.dimov1996@gmail.com>
2018-06-03 12:12:35 +0300

    updated Problem05LineNumbers.java in 09.Files And Directories - Exercises

--- Java-Fundamentals-May-2018/Java-Advanced-May-2018/09.Files And Directories - Exercises/src/Problem05LineNumbers.java
+++ Java-Fundamentals-May-2018/Java-Advanced-May-2018/09.Files And Directories - Exercises/src/Problem05LineNumbers.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Problem05LineNumbers {
     public static void main(String[] args) {
         String resources = "D:\\Soft\\SoftUni\\02.Software-University-SoftUni\\Software-University-SoftUni\\Java-Fundamentals-May-2018\\Java-Advanced-May-2018\\09.Files And Directories - Exercises\\Resources\\04. Java-Advanced-Files-and-Streams-Exercises-Resources";
-        String inputPathString = resources + "\\inputLineNumbers.txt ";
+        String inputPathString = resources + "\\inputLineNumbers.txt";
         String outputPathString = resources + "\\output.txt";
         Path inputPath = Paths.get(inputPathString);
         Path outputPath = Paths.get(outputPathString);
