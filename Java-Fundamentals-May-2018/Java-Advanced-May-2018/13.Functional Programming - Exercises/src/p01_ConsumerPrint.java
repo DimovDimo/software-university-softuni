a4ef4f66778a6de531691d01eba26aed448c5a73
DimovDimo <d.dimov1996@gmail.com>
2018-06-06 16:47:56 +0300

    added p01_ConsumerPrint.java in 13.Functional Programming - Exercises

---
+++ Java-Fundamentals-May-2018/Java-Advanced-May-2018/13.Functional Programming - Exercises/src/p01_ConsumerPrint.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class p01_ConsumerPrint {
+    public static void main(String[] args) throws IOException {
+        Consumer<List<String>> printSrings = list -> {
+            list.forEach(element -> System.out.println(element));
+        };
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = reader.readLine().split("\\s+");
+        List<String> words = new LinkedList<>();
+        Collections.addAll(words, inputs);
+        printSrings.accept(words);
+    }
+}
