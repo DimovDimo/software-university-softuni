41b291e839058be6d31a5126faff4bacdca8fd0c
DimovDimo <d.dimov1996@gmail.com>
2018-06-06 18:12:59 +0300

    added p04_FindEvensOrOdds.java in 13.Functional Programming - Exercises

---
+++ Java-Fundamentals-May-2018/Java-Advanced-May-2018/13.Functional Programming - Exercises/src/p04_FindEvensOrOdds.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.function.BiPredicate;
+
+public class p04_FindEvensOrOdds {
+    public static void main(String[] args) throws IOException {
+        BiPredicate<Integer, String> isTargetNumber = (num, condition) -> {
+            if ("odd".equals(condition) && num % 2 == 1){
+                return true;
+            } else if ("even".equals(condition) && num % 2 == 0){
+                return true;
+            } else {
+                return false;
+            }
+        };
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = reader.readLine().split("\\s+");
+        String state =reader.readLine();
+        int start = Integer.parseInt(inputs[0]);
+        int end = Integer.parseInt(inputs[1]);
+        for (int i = start; i <= end; i++) {
+            if (isTargetNumber.test(i, state)){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
