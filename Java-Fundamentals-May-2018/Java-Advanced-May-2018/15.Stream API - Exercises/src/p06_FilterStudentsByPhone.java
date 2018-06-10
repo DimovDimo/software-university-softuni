3392fb383310bd5cc1695c6b37ffd20f543628bb
DimovDimo <d.dimov1996@gmail.com>
2018-06-10 17:20:21 +0300

    added p06_FilterStudentsByPhone.java in 15.Stream API - Exercises

---
+++ Java-Fundamentals-May-2018/Java-Advanced-May-2018/15.Stream API - Exercises/src/p06_FilterStudentsByPhone.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class p06_FilterStudentsByPhone {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<Student> students = new ArrayList<>();
+        while (true){
+            String line = reader.readLine();
+            if ("END".equals(line)){
+                break;
+            }
+
+            String[] tokens = line.split("\\s+");
+            String firstName = tokens[0];
+            String lastName = tokens[1];
+            String phone = tokens[2];
+            if(isTargetPhone(phone)){
+                Student student = new Student(firstName, lastName);
+                students.add(student);
+            }
+        }
+
+        students.stream()
+                .forEach(System.out::println);
+    }
+
+    private static boolean isTargetPhone(String phone) {
+        if (phone.startsWith("02")||phone.startsWith("+3592")){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static class Student {
+        private String firstName;
+        private String lastName;
+
+        public Student(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s %s", this.firstName, this.lastName);
+        }
+    }
+}
+
+
