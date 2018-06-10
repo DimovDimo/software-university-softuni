9e5c0c057fa6a2e643de8573a553ba265740571f
DimovDimo <d.dimov1996@gmail.com>
2018-06-10 16:58:40 +0300

    added p04_SortStudents.java in 15.Stream API - Exercises

---
+++ Java-Fundamentals-May-2018/Java-Advanced-May-2018/15.Stream API - Exercises/src/p04_SortStudents.java
@@ -0,0 +1,53 @@
+import javafx.util.Pair;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class p04_SortStudents {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<Student> students = new ArrayList<>();
+        while (true){
+            String line = reader.readLine();
+            if ("END".equals(line)){
+                break;
+            }
+
+            String[] tokens = line.split("\\s+");
+            String firstName = tokens[0];
+            String lastName = tokens[1];
+
+            Student student = new Student(firstName, lastName);
+            students.add(student);
+        }
+
+        students.stream()
+                .sorted(Comparator.comparing(Student::getLastName).thenComparing(Student::getFirstName, Comparator.reverseOrder()))
+                .forEach(System.out::println);
+    }
+}
+
+class Student{
+    private String firstName;
+    private String lastName;
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", this.firstName, this.lastName);
+    }
+}
