9d86848d68f49ba04c732d23dd093e87d9173d2b
DimovDimo <d.dimov1996@gmail.com>
2018-06-20 11:47:02 +0300

    added p01_SingleInheritance in 07.Inheritance - Lab

---
+++ Java-Fundamentals-May-2018/Java-OOP-Basics-June-2018/07.Inheritance - Lab/src/p01_SingleInheritance/Animal.java
@@ -0,0 +1,7 @@
+package p01_SingleInheritance;
+
+public class Animal {
+    public void eat(){
+        System.out.println("eating…");
+    }
+}
---
+++ Java-Fundamentals-May-2018/Java-OOP-Basics-June-2018/07.Inheritance - Lab/src/p01_SingleInheritance/Dog.java
@@ -0,0 +1,7 @@
+package p01_SingleInheritance;
+
+public class Dog extends Animal {
+    public void bark(){
+        System.out.println("barking…");
+    }
+}
