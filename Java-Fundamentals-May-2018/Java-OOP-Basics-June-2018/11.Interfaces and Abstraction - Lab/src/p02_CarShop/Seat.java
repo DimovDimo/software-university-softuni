e67b3abc23717c444ef99d143647a988a39db578
DimovDimo <d.dimov1996@gmail.com>
2018-06-29 16:21:26 +0300

    added p02_CarShop in 11.Interfaces and Abstraction - Lab

---
+++ Java-Fundamentals-May-2018/Java-OOP-Basics-June-2018/11.Interfaces and Abstraction - Lab/src/p02_CarShop/Seat.java
@@ -0,0 +1,37 @@
+package p02_CarShop;
+
+import java.io.Serializable;
+
+public class Seat implements Car, Serializable {
+    private final String countryProduced;
+    private final String model;
+    private final String color;
+    private final int horsePower;
+
+    public Seat(String model, String color, int horsePower, String countryProduced) {
+        this.model = model;
+        this.color = color;
+        this.horsePower = horsePower;
+        this.countryProduced = countryProduced;
+    }
+
+    @Override
+    public String getModel() {
+        return this.model;
+    }
+
+    @Override
+    public String getColor() {
+        return this.color;
+    }
+
+    @Override
+    public int getHorsePower() {
+        return this.horsePower;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("This is %s produced in %s and have %d tires", this.getModel(), this.countryProduced, TIRES);
+    }
+}
