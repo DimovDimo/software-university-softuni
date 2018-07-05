9ed1ab76a9103c317dd287b6b0a8b287c0e2b0c7
DimovDimo <d.dimov1996@gmail.com>
2018-07-05 17:01:20 +0300

    added methods in Project structure OOP Basic exam

--- Java-Fundamentals-May-2018/Java-OOP-Basics-June-2018/Project structure OOP Basic exam/src/app/engines/Engine.java
+++ Java-Fundamentals-May-2018/Java-OOP-Basics-June-2018/Project structure OOP Basic exam/src/app/engines/Engine.java
@@ -1,4 +1,44 @@
 package app.engines;
 
+import app.io.ConsoleInputReader;
+import app.io.ConsoleOutputWriter;
+import app.utilities.InputParser;
+
+import java.util.List;
+
 public class Engine {
+    private ConsoleInputReader inputReader;
+    private ConsoleOutputWriter outputWriter;
+    private InputParser inputParser;
+//    private Manager manager;
+
+    public Engine(ConsoleInputReader inputReader, ConsoleOutputWriter outputWriter, InputParser inputParser, Manager manager) {
+        this.inputReader = inputReader;
+        this.outputWriter = outputWriter;
+        this.inputParser = inputParser;
+        this.manager = manager;
+    }
+
+    public void run(){
+        String inputLine;
+
+        while (true){
+            inputLine = this.inputReader.readLine();
+            List<String> commandParams = this.inputParser.parseInput(inputLine);
+
+            this.dispachCommand(commandParams);
+            //INPUT_TERMINATING_COMMAND - this is your terminared command
+            if (inputLine.equals("INPUT_TERMINATING_COMMAND")){
+                break;
+            }
+        }
+    }
+
+    private void dispachCommand(List<String> commandParams) {
+        String command = commandParams.remove(0);
+
+        switch (command){
+
+        }
+    }
 }
--- Java-Fundamentals-May-2018/Java-OOP-Basics-June-2018/Project structure OOP Basic exam/src/app/io/ConsoleOutputWriter.java
+++ Java-Fundamentals-May-2018/Java-OOP-Basics-June-2018/Project structure OOP Basic exam/src/app/io/ConsoleOutputWriter.java
@@ -1,4 +1,8 @@
 package app.io;
 
 public class ConsoleOutputWriter {
+	    
+    public void writeLine(String output){
+        System.out.println(output);
+    }
 }
