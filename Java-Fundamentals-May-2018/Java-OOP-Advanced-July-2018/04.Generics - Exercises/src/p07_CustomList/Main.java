8a2de7ab6108249f73f6ede97ca1551a209d6855
DimovDimo <d.dimov1996@gmail.com>
2018-07-12 21:06:11 +0300

    added p07_CustomList in 04.Generics - Exercises

---
+++ Java-Fundamentals-May-2018/Java-OOP-Advanced-July-2018/04.Generics - Exercises/src/p07_CustomList/Main.java
@@ -0,0 +1,63 @@
+package p07_CustomList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        MyList<String> customList = new MyArrayList<>();
+        while (true){
+            String line = reader.readLine();
+            if ("END".equals(line)){
+                break;
+            }
+
+            String[] tokens = line.split("\\s+");
+
+            String element;
+            int firstIndex;
+            int secondIndex;
+            Object result = null;
+
+            switch (tokens[0]){
+                case "Add":
+                    element = tokens[1];
+                    customList.add(element);
+                    break;
+                case "Remove":
+                    firstIndex = Integer.parseInt(tokens[1]);
+                    customList.remove(firstIndex);
+                    break;
+                case "Contains":
+                    element = tokens[1];
+                    result = String.valueOf(customList.contains(element));
+                    break;
+                case "Swap":
+                    firstIndex = Integer.parseInt(tokens[1]);
+                    secondIndex = Integer.parseInt(tokens[2]);
+                    customList.swap(firstIndex, secondIndex);
+                    break;
+                case "Greater":
+                    element = tokens[1];
+                    result = String.valueOf(customList.countGreaterThan(element));
+                    break;
+                case "Max":
+                    result = customList.getMax();
+                    break;
+                case "Min":
+                    result = customList.getMin();
+                    break;
+                case "Print":
+                    result = customList.toString();
+                    break;
+            }
+
+            if (result != null){
+                System.out.println(
+                        String.valueOf(result));
+            }
+        }
+    }
+}
---
+++ Java-Fundamentals-May-2018/Java-OOP-Advanced-July-2018/04.Generics - Exercises/src/p07_CustomList/MyList.java
@@ -0,0 +1,17 @@
+package p07_CustomList;
+
+public interface MyList<T extends Comparable<T>> {
+    void add(T element);
+
+    T remove(int index);
+
+    boolean contains(T element);
+
+    void swap(int firstIndex, int secondIndex);
+
+    int countGreaterThan(T element);
+
+    T getMax();
+
+    T getMin();
+}
