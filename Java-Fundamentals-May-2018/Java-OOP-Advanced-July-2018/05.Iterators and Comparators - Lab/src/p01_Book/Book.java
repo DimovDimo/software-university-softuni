e0f412514fb2455b47d17fe7b2bc46820a0ce31e
DimovDimo <d.dimov1996@gmail.com>
2018-07-14 10:24:17 +0300

    added 05.Iterators and Comparators - Lab in Java-OOP-Advanced-July-2018

---
+++ Java-Fundamentals-May-2018/Java-OOP-Advanced-July-2018/05.Iterators and Comparators - Lab/src/p01_Book/Book.java
@@ -0,0 +1,46 @@
+package p01_Book;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Book {
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        this.setTitle(title);
+        this.setYear(year);
+        this.setAuthors(authors);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    private void setYear(int year) {
+        this.year = year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    private void setAuthors(String... authors) {
+        if (authors.length == 0){
+            this.authors = new ArrayList<>();
+        } else {
+            this.authors = Arrays.stream(authors).collect(Collectors.toList());
+        }
+    }
+}
---
+++ Java-Fundamentals-May-2018/Java-OOP-Advanced-July-2018/05.Iterators and Comparators - Lab/src/p02_Library/Library.java
@@ -0,0 +1,30 @@
+package p02_Library;
+
+import java.util.Iterator;
+
+public class Library<Book> implements Iterable<Book> {
+    private Book[] books;
+
+    public Library(Book... books) {
+        this.books = books;
+    }
+
+    @Override
+    public Iterator<Book> iterator() {
+        return new LibraryIterator();
+    }
+
+    private final class LibraryIterator implements Iterator<Book>{
+        private int counter = 0;
+
+        @Override
+        public boolean hasNext() {
+            return this.counter < books.length;
+        }
+
+        @Override
+        public Book next() {
+            return books[counter++];
+        }
+    }
+}
