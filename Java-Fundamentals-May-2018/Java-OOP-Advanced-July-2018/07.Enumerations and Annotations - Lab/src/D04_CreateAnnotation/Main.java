059a11ec6cd09c9293e5be8f40d48b97cdd1b653
DimovDimo <d.dimov1996@gmail.com>
2018-07-18 18:46:55 +0300

    added D04_CreateAnnotation in 07.Enumerations and Annotations - Lab

---
+++ Java-Fundamentals-May-2018/Java-OOP-Advanced-July-2018/07.Enumerations and Annotations - Lab/src/D04_CreateAnnotation/Main.java
@@ -0,0 +1,7 @@
+package D04_CreateAnnotation;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Java-Fundamentals-May-2018/Java-OOP-Advanced-July-2018/07.Enumerations and Annotations - Lab/src/D04_CreateAnnotation/TestClass.java
@@ -0,0 +1,8 @@
+package D04_CreateAnnotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Subject(categories = {"Test", "Annotations"})
+public class TestClass {
+}
