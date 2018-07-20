1eec080603c32f9c9f323fac49963918bbf4970f
DimovDimo <d.dimov1996@gmail.com>
2018-07-20 10:40:11 +0300

    added L12_PrimeNumberChecker in 04.Lab Control-Flow Logic

---
+++ JS-Core/JS-Fundamentals-May-2018/04.Lab Control-Flow Logic/Work/L12_PrimeNumberChecker/PrimeNumberChecker.js
@@ -0,0 +1,8 @@
+function primeNumberChecker(number) {
+    let start = 2;
+    const limit = Math.sqrt(number);
+    while (start <= limit) {
+        if (number % start++ < 1) return false;
+    }
+    return number > 1;
+}
