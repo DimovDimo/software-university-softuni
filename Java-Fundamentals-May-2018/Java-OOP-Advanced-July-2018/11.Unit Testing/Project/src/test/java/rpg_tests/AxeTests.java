85282aca23ecc7e4fd5b7f7dbda8a01f6b1f0268
DimovDimo <d.dimov1996@gmail.com>
2018-07-26 14:56:46 +0300

    refactored code of AxeTests.java in 11.Unit Testing

--- Java-Fundamentals-May-2018/Java-OOP-Advanced-July-2018/11.Unit Testing/Project/src/test/java/rpg_tests/AxeTests.java
+++ Java-Fundamentals-May-2018/Java-OOP-Advanced-July-2018/11.Unit Testing/Project/src/test/java/rpg_tests/AxeTests.java
@@ -1,24 +1,37 @@
 package rpg_tests;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import rpg_lab.Axe;
 import rpg_lab.Dummy;
 
 public class AxeTests {
+    public static final int AXE_ATTACK = 10;
+    public static final int AXE_DURABILITY = 10;
+    public static final int DUMMY_HEALTH = 10;
+    public static final int DUMMY_XP = 10;
+    public static final int EXPECTED_DURABILITY = AXE_DURABILITY - 1;
+    public static final int ZERO = 0;
+
+    private Axe axe;
+    private Dummy dummy;
+
+    @Before
+    public void initializedTestIbjects(){
+        this.axe = new Axe(AXE_ATTACK, AXE_DURABILITY);
+        this.dummy = new Dummy(DUMMY_HEALTH, DUMMY_XP);
+    }
 
     @Test
     public void weaponLosesDurability(){
-        Axe axe = new Axe(10, 10);
-        Dummy dummy = new Dummy(10, 10);
-        axe.attack(dummy);
-        Assert.assertEquals(9, axe.getDurabilityPoints());
+        this.axe.attack(this.dummy);
+        Assert.assertEquals("Wrong Durability", EXPECTED_DURABILITY, this.axe.getDurabilityPoints());
     }
 
     @Test(expected = IllegalStateException.class)
     public void attackingWithABrokenWeapon(){
-        Axe axe = new Axe(10, 0);
-        Dummy dummy = new Dummy(10, 10);
-        axe.attack(dummy);
+        Axe axeWithBokenWeapon = new Axe(AXE_ATTACK, ZERO);
+        axeWithBokenWeapon.attack(dummy);
     }
 }
