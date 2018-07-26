5f96fbd67abbbb9b3835f37412d1e3f9617d0241
DimovDimo <d.dimov1996@gmail.com>
2018-07-26 15:09:50 +0300

    refactored code of DummyTests.java in 11.Unit Testing

--- Java-Fundamentals-May-2018/Java-OOP-Advanced-July-2018/11.Unit Testing/Project/src/test/java/rpg_tests/DummyTests.java
+++ Java-Fundamentals-May-2018/Java-OOP-Advanced-July-2018/11.Unit Testing/Project/src/test/java/rpg_tests/DummyTests.java
@@ -1,33 +1,45 @@
 package rpg_tests;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import rpg_lab.Dummy;
 
 public class DummyTests {
 
+    public static final int HEALTH = 10;
+    public static final int EXPERIENCE = 10;
+    public static final int ATTACK_POINTS = 10;
+    public static final int EXPECTED_HEALTH = HEALTH - ATTACK_POINTS;
+    public static final int EXPECTED_EXPERIENCE = EXPERIENCE;
+
+    private Dummy dummy;
+
+    @Before
+    public void initializedTestIbjectDummy(){
+        this.dummy = new Dummy(HEALTH, EXPERIENCE);
+    }
+
     @Test
     public void losesHealthIfAttacked(){
-        Dummy dummy = new Dummy(10, 10);
-        dummy.takeAttack(1);
-        Assert.assertEquals(9, dummy.getHealth());
+        this.dummy.takeAttack(ATTACK_POINTS);
+        Assert.assertEquals("Wrong Health", EXPECTED_HEALTH, this.dummy.getHealth());
     }
 
     @Test(expected = IllegalStateException.class)
     public void deadDummyThrowsExceptionIfAttacked(){
-        Dummy dummy = new Dummy(0, 10);
-        dummy.takeAttack(1);
+        this.dummy.takeAttack(ATTACK_POINTS);
+        this.dummy.takeAttack(ATTACK_POINTS);
     }
 
     @Test
     public void deadDummyCanGiveXP(){
-        Dummy dummy = new Dummy(0, 10);
-        Assert.assertEquals(10, dummy.giveExperience());
+        this.dummy.takeAttack(ATTACK_POINTS);
+        Assert.assertEquals("Wrong XP", EXPECTED_EXPERIENCE, this.dummy.giveExperience());
     }
 
     @Test(expected = IllegalStateException.class)
     public void aliveDummyCanNotGiveXP(){
-        Dummy dummy = new Dummy(10, 10);
-        dummy.giveExperience();
+        this.dummy.giveExperience();
     }
 }
