fc47511546d9d073f888e192a42265cf8d2542e7
DimovDimo <d.dimov1996@gmail.com>
2018-08-12 19:42:54 +0300

    added A01_Subtraction in 05.DOM Manipulation

---
+++ JS-Core/JS-Advanced-June-2018/05.DOM Manipulation/Work/A01_Subtraction/subtract.js
@@ -0,0 +1,5 @@
+function subtract() {
+    let firstNum = document.getElementById('firstNumber').value;
+    let secondNum = document.getElementById('secondNumber').value;
+    document.getElementById('result').textContent = Number(firstNum) - Number(secondNum);
+}
