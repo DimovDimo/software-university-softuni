c7a76f1734fdec6dac1f7eb1c00655127868c966
DimovDimo <d.dimov1996@gmail.com>
2018-08-26 21:54:05 +0300

    added A01_ModifyABit in Bitwise Operations for Java-Advanced-May-2018

---
+++ Java-Fundamentals-May-2018/Java-Advanced-May-2018/Bitwise Operations/src/A01_ModifyABit/ModifyABit.java
@@ -0,0 +1,21 @@
+package A01_ModifyABit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ModifyABit {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] tokens = reader.readLine().split(" ");
+
+        int number = Integer.parseInt(tokens[0]);
+        int position = Integer.parseInt(tokens[1]);
+
+        int shift = number >> position;
+        int bit = shift & 1;
+
+        System.out.println(bit);
+    }
+}
