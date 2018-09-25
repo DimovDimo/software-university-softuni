5c44b78a82b82114ae768d9532a48c3a09607248
DimovDimo <d.dimov1996@gmail.com>
2018-09-25 18:07:44 +0300

    added 01. Create Tables in 04.Data Definition and Data Types - Exercise

--- Java-DB-September-2018/Database-Basics-MySQL-September-2018/04.Data Definition and Data Types - Exercise/Work/Data Definition and Data Types - Exercise.sql
+++ Java-DB-September-2018/Database-Basics-MySQL-September-2018/04.Data Definition and Data Types - Exercise/Work/Data Definition and Data Types - Exercise.sql
@@ -1,3 +1,16 @@
 /*Create Database*/
 CREATE DATABASE minions;
 
+USE minions;
+
+/*01. Create Tables*/
+CREATE TABLE minions(
+	id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    age INT
+);
+
+CREATE TABLE towns (
+	id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+);
