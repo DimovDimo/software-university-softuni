68783a5f6cc20b2a4c98ee24c020b0ecd52508c1
DimovDimo <d.dimov1996@gmail.com>
2018-09-25 18:20:41 +0300

    added 02. Alter Minions Table in 04.Data Definition and Data Types - Exercise

--- Java-DB-September-2018/Database-Basics-MySQL-September-2018/04.Data Definition and Data Types - Exercise/Work/Data Definition and Data Types - Exercise.sql
+++ Java-DB-September-2018/Database-Basics-MySQL-September-2018/04.Data Definition and Data Types - Exercise/Work/Data Definition and Data Types - Exercise.sql
@@ -14,3 +14,12 @@ CREATE TABLE towns (
 	id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL
 );
+
+/*02. Alter Minions Table */
+ALTER TABLE minions
+ADD town_id INT;
+
+ALTER TABLE minions
+ADD CONSTRAINT fk_towns_id
+FOREIGN KEY (town_id)
+REFERENCES towns(id);
