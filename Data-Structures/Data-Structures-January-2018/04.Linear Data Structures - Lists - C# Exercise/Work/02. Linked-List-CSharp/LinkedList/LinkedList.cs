4aa683eaa841f67cf61002a4f24b02a9f05bd9af
DimovDimo <d.dimov1996@gmail.com>
2018-09-28 19:01:43 +0300

    added Information in 04.Linear Data Structures - Lists - C# Exercise for Data-Structures-January-2018

---
+++ Data-Structures/Data-Structures-January-2018/04.Linear Data Structures - Lists - C# Exercise/Work/02. Linked-List-CSharp/LinkedList/LinkedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LinkedList<T> : IEnumerable<T>
+{
+    public int Count { get; private set; }
+
+    public void AddFirst(T item)
+    {
+        // TODO
+        throw new NotImplementedException();
+    }
+
+    public void AddLast(T item)
+    {
+        // TODO
+        throw new NotImplementedException();
+    }
+
+    public T RemoveFirst()
+    {
+        // TODO: Throw exception if the list is empty
+        throw new NotImplementedException();
+    }
+
+    public T RemoveLast()
+    {
+        // TODO: Throw exception if the list is empty
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        // TODO
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        // TODO
+        throw new NotImplementedException();
+    }
+}
