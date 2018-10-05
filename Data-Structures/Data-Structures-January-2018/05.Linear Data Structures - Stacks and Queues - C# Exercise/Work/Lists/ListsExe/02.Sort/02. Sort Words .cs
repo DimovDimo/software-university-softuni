457e0ad9825f47c15c4780017d324504eaed1b9e
DimovDimo <d.dimov1996@gmail.com>
2018-10-05 16:31:55 +0300

    added problems 1 & 2 in 05.Linear Data Structures - Stacks and Queues - C# Exercise for Data-Structures-January-2018

---
+++ Data-Structures/Data-Structures-January-2018/05.Linear Data Structures - Stacks and Queues - C# Exercise/Work/Lists/ListsExe/02.Sort/02. Sort Words .cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine()
+                .Split(' ')
+                .ToList();
+
+            words.Sort();
+
+            Console.WriteLine(String.Join(" ", words));
+        }
+    }
+}
---
+++ Data-Structures/Data-Structures-January-2018/05.Linear Data Structures - Stacks and Queues - C# Exercise/Work/Lists/ListsExe/03/03. Longest Subsequence .cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                .Split(' ')
+                .Select(s => int.Parse(s))
+                .ToList();
+
+            List<int> longestSubsequence = new List<int>();
+            List<int> currentSubsequence = new List<int>();
+            int lastElement = 0;
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                
+            }
+
+            if (longestSubsequence.Count < currentSubsequence.Count)
+            {
+                longestSubsequence = currentSubsequence;
+            }
+
+            Console.WriteLine(String.Join(" ", longestSubsequence));
+        }
+    }
+}
