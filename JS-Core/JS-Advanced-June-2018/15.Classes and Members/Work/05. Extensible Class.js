8a133e133a81f5e317f33039e5f6b62b7d7d7354
DimovDimo <d.dimov1996@gmail.com>
2018-10-05 18:22:03 +0300

    added 05. Extensible Class.js in 15.Classes and Members for JS-Advanced-June-2018

---
+++ JS-Core/JS-Advanced-June-2018/15.Classes and Members/Work/05. Extensible Class.js
@@ -0,0 +1,19 @@
+(function () {
+    let id = 0;
+
+    return class Extensible {
+        constructor(){
+            this.id = id++;
+        }
+
+        extend(template){
+            for(let prop in template){
+                if (typeof  template[prop] === 'function'){
+                    Extensible.prototype[prop] = template[prop];
+                } else {
+                    this[prop] = template[prop];
+                }
+            }
+        }
+    }
+})()
