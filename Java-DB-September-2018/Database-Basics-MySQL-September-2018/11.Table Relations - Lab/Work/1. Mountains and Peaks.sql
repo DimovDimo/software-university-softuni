2bb98b1234707d2e54c26829f2131582218bd3e3
DimovDimo <d.dimov1996@gmail.com>
2018-10-06 19:19:24 +0300

    added 1. Mountains and Peaks.sql in 11.Table Relations - Lab for Database-Basics-MySQL-September-2018

---
+++ Java-DB-September-2018/Database-Basics-MySQL-September-2018/11.Table Relations - Lab/Work/1. Mountains and Peaks.sql
@@ -0,0 +1,15 @@
+USE camp;
+
+CREATE TABLE mountains(
+	id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50)
+);
+
+CREATE TABLE peaks(
+	id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    mountain_id INT(11) NOT NULL,
+    CONSTRAINT fk_peaks_mountains
+    FOREIGN KEY (mountain_id)
+    REFERENCES mountains(id)
+);
