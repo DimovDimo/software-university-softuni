734df167f6577410ae06c77452c800ee3d636666
DimovDimo <d.dimov1996@gmail.com>
2018-10-07 19:47:59 +0300

    added 03. Many-To-Many Relationship.sql in 12.Table Relations - Exercise for Database-Basics-MySQL-September-2018

---
+++ Java-DB-September-2018/Database-Basics-MySQL-September-2018/12.Table Relations - Exercise/Work/03. Many-To-Many Relationship.sql
@@ -0,0 +1,48 @@
+USE camp;
+
+CREATE TABLE students(
+	student_id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(20)
+);
+
+CREATE TABLE exams(
+	exam_id INT PRIMARY KEY,
+	name VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE students_exams(
+	student_id INT,
+    exam_id INT,
+    
+    CONSTRAINT pk_students_exams
+    PRIMARY KEY (student_id, exam_id),
+    
+    CONSTRAINT fk_student_id
+    FOREIGN KEY (student_id)
+    REFERENCES students(student_id),
+    
+    CONSTRAINT fk_exam_id
+    FOREIGN KEY (exam_id)
+    REFERENCES exams(exam_id)
+);
+
+INSERT INTO students(name)
+VALUES 
+	('Mila'),
+    ('Toni'),
+    ('Ron');
+
+INSERT INTO exams(exam_id,name)
+VALUES
+	(101,'Spring MVC'),
+    (102,'Neo4j'),
+    (103,'Oracle 11g');
+
+INSERT INTO students_exams(student_id,exam_id)
+VALUES 
+	(1,101),
+    (1,102),
+    (2,101),
+    (3,103),
+    (2,102),
+    (2,103);
