3931e0ca960b5cb9a4bc5888c98679f06bbbf90b
DimovDimo <d.dimov1996@gmail.com>
2018-10-10 16:43:24 +0300

    added BinarySearchTree in 07.Binary Search Trees - C# Lab for Data-Structures-January-2018

---
+++ Data-Structures/Data-Structures-January-2018/07.Binary Search Trees - C# Lab/Work/Trees/Trees/BinarySearchTree.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BinarySearchTree<T> where T : IComparable<T>
+{
+
+    private Node root;
+
+    private class Node
+    {
+        public Node(T value)
+        {
+            this.Value = value;
+            this.Left = null;
+            this.Right = null;
+        }
+
+        public T Value { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+    }
+
+    public BinarySearchTree()
+    {
+        this.root = null;
+    }
+
+    public void Insert(T element)
+    {
+        this.root = this.Insert(element, this.root);
+    }
+
+    private Node Insert(T element, Node node)
+    {
+        if (node == null)
+        {
+            node = new Node(element);
+        }
+        else if (element.CompareTo(node.Value) < 0)
+        {
+            node.Left = this.Insert(element, node.Left);
+        }
+        else if (element.CompareTo(node.Value) > 0)
+        {
+            node.Right = this.Insert(element, node.Right);
+        }
+        
+        return node;
+    }
+
+    public bool Contains(T value)
+    {
+        Node current = this.root;
+        while (current != null)
+        {
+            if (value.CompareTo(current.Value) < 0)
+                current = current.Left;
+            else if (value.CompareTo(current.Value) > 0)
+                current = current.Right;
+            else
+                break;
+        }
+        return current != null;
+    }
+
+    public void DeleteMin()
+    {
+        if (this.root == null)
+        {
+            return;
+        }
+
+        if (this.root.Left == null && this.root.Right == null)
+        {
+            this.root = null;
+            return;
+        }
+
+        Node parent = null;
+        Node current = this.root;
+
+        while(current.Left != null)
+        {
+            parent = current;
+            current = current.Left;
+        }
+
+        if (current.Right != null)
+        {
+            parent.Left = current.Right;
+        }
+        else
+        {
+            parent.Left = null;
+        }
+    }
+
+    public BinarySearchTree<T> Search(T item)
+    {
+        Node current = this.root;
+        while (current != null)
+        {
+            if (item.CompareTo(current.Value) < 0)
+                current = current.Left;
+            else if (item.CompareTo(current.Value) > 0)
+                current = current.Right;
+            else
+                break;
+        }
+        return new BinarySearchTree<T>(current);
+    }
+
+    private BinarySearchTree(Node root)
+    {
+        this.Copy(root);
+    }
+
+    private void Copy(Node node)
+    {
+        if (node == null) return;
+
+        this.Insert(node.Value);
+        this.Copy(node.Left);
+        this.Copy(node.Right);
+    }
+
+    public IEnumerable<T> Range(T startRange, T endRange)
+    {
+        List<T> result = new List<T>();
+
+        this.Range(this.root, result, startRange, endRange);
+
+        return result;
+    }
+
+    private void Range(Node node, List<T> result, T startRange, T endRange)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        int compareLow = node.Value.CompareTo(startRange);
+        int compareHigh = node.Value.CompareTo(endRange);
+
+        if (compareLow > 0)
+        {
+            this.Range(node.Left, result, startRange, endRange);
+        }
+        if (compareLow >= 0 && compareHigh <= 0)
+        {
+            result.Add(node.Value);
+        }
+        if (compareHigh < 0)
+        {
+            this.Range(node.Right, result, startRange, endRange);
+        }
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        this.EachInOrder(this.root, action);
+    }
+
+    private void EachInOrder(Node node, Action<T> action)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        this.EachInOrder(node.Left, action);
+        action(node.Value);
+        this.EachInOrder(node.Right, action);
+    }
+}
+
+public class Launcher
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
