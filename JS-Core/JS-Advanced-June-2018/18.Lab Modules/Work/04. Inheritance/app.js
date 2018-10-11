bfb8ba177e67e846aefd9a888c03b0ca5193d0cc
DimovDimo <d.dimov1996@gmail.com>
2018-10-11 19:11:59 +0300

    added 04. Inheritance in 18.Lab Modules for JS-Advanced-June-2018

---
+++ JS-Core/JS-Advanced-June-2018/18.Lab Modules/Work/04. Inheritance/app.js
@@ -0,0 +1,9 @@
+let Entity = require('./entity');
+let Person = require('./person');
+let Dog = require('./dog');
+let Student = require('./student');
+
+result.Entity = Entity;
+result.Person = Person;
+result.Dog = Dog;
+result.Student = Student;
