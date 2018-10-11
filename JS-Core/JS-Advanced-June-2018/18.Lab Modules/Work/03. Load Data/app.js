bdf8d4cde07567b8695b660bcc84ff460dcceee2
DimovDimo <d.dimov1996@gmail.com>
2018-10-11 19:10:28 +0300

    added 03. Load Data  in 18.Lab Modules for JS-Advanced-June-2018

---
+++ JS-Core/JS-Advanced-June-2018/18.Lab Modules/Work/03. Load Data/app.js
@@ -0,0 +1,13 @@
+let data = require('./data');
+
+function sort(property) {
+    data.sort((a,b)=>a[property].localeCompare(b[property]));
+    return data
+}
+
+function filter(property, value) {
+    return data.filter(a => a[property] === value);
+}
+
+result.sort = sort;
+result.filter = filter;
