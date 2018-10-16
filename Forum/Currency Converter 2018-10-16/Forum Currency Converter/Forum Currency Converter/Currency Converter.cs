3b0c4cda382846044eca00b3bfc6a1a39a70d93c
DimovDimo <d.dimov1996@gmail.com>
2018-10-16 20:38:45 +0300

    added Forum Currency Converter in Forum

---
+++ Forum/Currency Converter 2018-10-16/Forum Currency Converter/Forum Currency Converter/Currency Converter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        const double usd = 1.79549;
+        const double eur = 1.95583;
+        const double gbp = 2.53405;
+
+        double money = double.Parse(Console.ReadLine());
+        string firstCurrency = Console.ReadLine();
+        string secondCurrency = Console.ReadLine();
+
+        switch (firstCurrency)
+        {
+            case "USD": money = money * usd; break;
+            case "EUR": money = money * eur; break;
+            case "GBP": money = money * gbp; break;
+        }
+
+        switch (secondCurrency)
+        {
+            case "USD": money = money / usd; break;
+            case "EUR": money = money / eur; break;
+            case "GBP": money = money / gbp; break;
+        }
+
+        Console.WriteLine($"{money:F2} {secondCurrency}");
+    }
+}
