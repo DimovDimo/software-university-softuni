1a62ea3733c0823ed4354bbc2b30f3aa2f7de496
DimovDimo <d.dimov1996@gmail.com>
2018-10-20 20:59:38 +0300

    added all problems in Databases MySQL Exam Preparation II for Database-Basics-MySQL-September-2018

--- Java-DB-September-2018/Database-Basics-MySQL-September-2018/Databases MySQL Exam Preparation II/Work/Databases MySQL Exam Preparation II.sql
+++ Java-DB-September-2018/Database-Basics-MySQL-September-2018/Databases MySQL Exam Preparation II/Work/Databases MySQL Exam Preparation II.sql
@@ -84,7 +84,41 @@ CREATE TABLE users_chats
 
 # 02. Insert 
 
+INSERT INTO messages(
+					content,
+					sent_on,
+					chat_id,
+					user_id)
+	SELECT 
+		concat_ws('-', u.age, u.gender, l.latitude, l.longitude) AS content,
+        '2016-12-15' AS sent_on,
+        (CASE
+			WHEN u.gender = 'F' THEN ceil(sqrt(u.age * 2))
+            WHEN u.gender = 'M' THEN (pow((u.age /18), 3))
+        END) AS chat_id,
+        u.id AS user_id 
+	FROM users AS u
+    JOIN locations AS l ON l.id = u.location_id
+	WHERE u.id BETWEEN 10 AND 20;
+
 # 03. Update 
+
+UPDATE chats AS c
+JOIN
+	(
+		SELECT 
+			c.id AS chat_id,
+			c.start_date AS start_date,
+			m.id AS message_id, 
+			m.sent_on AS sent_on
+		FROM chats AS c
+		JOIN messages AS m ON m.chat_id = c.id
+		WHERE c.start_date > m.sent_on
+		GROUP BY c.id
+		ORDER BY m.sent_on, c.id
+    ) AS d ON c.id = d.chat_id
+SET c.start_date = d.sent_on
+WHERE c.id = d.chat_id;
 
 # 04. Delete
 
@@ -153,6 +187,14 @@ ORDER BY id;
 
 # 12. Left Users 
 
+SELECT m.id, m.chat_id, m.user_id
+FROM messages AS m
+LEFT JOIN users_chats AS uc ON uc.chat_id = m.chat_id
+	AND uc.user_id = m.user_id
+WHERE m.chat_id = 17
+	AND uc.user_id IS NULL
+ORDER BY m.id DESC;
+
 # 13. Users in Bulgaria 
 
 SELECT 
@@ -167,6 +209,19 @@ JOIN chats AS c ON c.id = uc.chat_id
 WHERE (l.latitude BETWEEN 41.139999 AND 44.129999)
 	AND (l.longitude BETWEEN 22.209999 AND 28.359999)
 ORDER BY c.title;
+
+# 14. Last Chat 
+
+SELECT c.title, m.content
+FROM chats AS c
+LEFT JOIN messages AS m ON m.chat_id = c.id
+WHERE c.start_date = (
+						SELECT start_date
+                        FROM chats
+                        ORDER BY start_date DESC
+                        LIMIT 1
+                        )
+ORDER BY m.sent_on, m.id;
 
 # Section 4: Programmability – 40 pts
 
@@ -216,3 +271,96 @@ END $$
 DELIMITER ;
 
 CALL udp_change_password('abarnes0@sogou.com', 'new_pass');
+
+# 17. Send Message 
+
+DROP PROCEDURE IF EXISTS udp_send_message;
+
+DELIMITER $$
+CREATE PROCEDURE udp_send_message(
+	user_id INT(11),
+    chat_id INT(11),
+    content VARCHAR(200))
+BEGIN
+	
+    DECLARE exists_user_id_and_chat_id INT DEFAULT (SELECT uc.user_id FROM users_chats AS uc WHERE uc.user_id = user_id AND uc.chat_id = chat_id);
+    
+	START TRANSACTION;
+	IF(exists_user_id_and_chat_id IS NULL) THEN
+		SIGNAL SQLSTATE '45000' 
+			SET MESSAGE_TEXT = 'There is no chat with that user!';
+		ROLLBACK;
+    ELSE   
+		INSERT INTO messages(content,
+								sent_on,
+								chat_id,
+								user_id)
+		VALUES (content, '2016-12-15', chat_id, user_id);
+        
+        COMMIT;
+    END IF;
+END $$
+DELIMITER ;
+
+CALL udp_send_message(19, 17, 'Awesome');
+
+# 18. Log Messages 
+
+DROP TRIGGER IF EXISTS tr_log_messages;
+DROP TABLE IF EXISTS messages_log;
+
+CREATE TABLE messages_log
+(
+	id INT(11) NOT NULL PRIMARY KEY,
+    content VARCHAR(200),
+    sent_on DATE,
+    chat_id INT(11),
+    user_id INT(11),
+    
+    CONSTRAINT fk_messages_log_chats
+    FOREIGN KEY (chat_id)
+    REFERENCES chats(id),
+    
+    CONSTRAINT fk_messages_log_users
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+);
+
+DELIMITER $$
+CREATE TRIGGER tr_log_messages 
+AFTER DELETE ON messages
+FOR EACH ROW
+BEGIN
+	INSERT INTO 
+			messages_log(
+					id,
+					content,
+					sent_on,
+					chat_id,
+					user_id)
+		VALUES (
+				OLD.id,
+				OLD.content,
+				OLD.sent_on,
+				OLD.chat_id,
+				OLD.user_id);
+END $$
+DELIMITER ;
+
+DELETE FROM messages
+WHERE id = 1;
+
+# 19. Delete Users 
+
+DROP TRIGGER IF EXISTS tr_users_before_deleted;
+
+DELIMITER $$
+CREATE TRIGGER tr_users_before_deleted
+BEFORE DELETE ON users
+FOR EACH ROW
+BEGIN
+	DELETE uc FROM users_chats AS uc WHERE uc.user_id = OLD.id;
+    DELETE m FROM messages AS m WHERE m.user_id = OLD.id;
+    DELETE ml FROM messages_log AS ml WHERE ml.user_id = OLD.id;
+END $$
+DELIMITER ;
