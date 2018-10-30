d304122f47488fc65f202bf35b2d138531a0090c
DimovDimo <d.dimov1996@gmail.com>
2018-10-30 17:31:00 +0200

    added 08. Data-Structures-Ropes-and-Tries-Exercise-CSharp in 14.Avl And Aa-Trees, Rope And Trie - Exercise for Data-Structures-January-2018

---
+++ Data-Structures/Data-Structures-January-2018/14.Avl And Aa-Trees, Rope And Trie - Exercise/Work/08. Data-Structures-Ropes-and-Tries-Exercise-CSharp/Text Editor/Text Editor/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        TextEditor textEditor = new TextEditor();
+        while (true)
+        {
+            var input = Console.ReadLine();
+            var text = input.Split('"');
+            var commands = input.Split();
+            if (commands[0] == "end")
+            {
+                break;
+            }
+
+            switch (commands[0])
+            {
+                case "login":
+                    textEditor.Login(commands[1]);
+                    break;
+                case "logout":
+                    textEditor.Logout(commands[1]);
+                    break;
+                case "users":
+                    if (commands.Length > 1)
+                    {
+                        foreach (var user in textEditor.Users(commands[1]))
+                        {
+                            Console.WriteLine(user);
+                        }
+                    }
+                    else
+                    {
+                        foreach (var user in textEditor.Users())
+                        {
+                            Console.WriteLine(user);
+                        }
+                    }
+                    break;
+                default:
+                    var name = commands[0];
+                    var command = commands[1];
+
+                    switch (command)
+                    {
+                        case "insert":
+                            var index = int.Parse(commands[2]);
+                            textEditor.Insert(name, index, text[1]);
+                            break;
+                        case "prepend":
+                            textEditor.Prepend(name, text[1]);
+                            break;
+                        case "substring":
+                            var startIndex = int.Parse(commands[2]);
+                            var length = int.Parse(commands[3]);
+                            textEditor.Substring(name, startIndex, length);
+                            break;
+                        case "delete":
+                            var startIndexD = int.Parse(commands[2]);
+                            var lengthD = int.Parse(commands[3]);
+                            textEditor.Delete(name, startIndexD, lengthD);
+                            break;
+                        case "clear":
+                            textEditor.Clear(name);
+                            break;
+                        case "length":
+                            Console.WriteLine(textEditor.Length(name));
+                            break;
+                        case "print":
+                            try
+                            {
+                                Console.WriteLine(textEditor.Print(name));
+                            }
+                            catch (Exception)
+                            {
+                            }
+                            break;
+                        case "undo":
+                            textEditor.Undo(name);
+                            break;
+                        default: throw new InvalidOperationException();
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ Data-Structures/Data-Structures-January-2018/14.Avl And Aa-Trees, Rope And Trie - Exercise/Work/08. Data-Structures-Ropes-and-Tries-Exercise-CSharp/Text Editor/Text Editor/TextEditor.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Wintellect.PowerCollections;
+
+class TextEditor : ITextEditor
+{
+    private Trie<BigList<char>> UsersText { get; set; }
+
+    private Trie<Stack<string>> UsersStack { get; set; }
+
+    public TextEditor()
+    {
+        this.UsersText = new Trie<BigList<char>>();
+        this.UsersStack = new Trie<Stack<string>>();
+    }
+
+    public void Login(string username)
+    {
+        this.UsersText.Insert(username, new BigList<char>());
+        this.UsersStack.Insert(username, new Stack<string>());
+    }
+
+    public void Logout(string username)
+    {
+        this.UsersText.Delete(username);
+        this.UsersStack.Delete(username);
+    }
+
+    public void Prepend(string username, string str)
+    {
+        if (!this.UsersStack.Contains(username))
+        {
+            return;
+        }
+
+        var stack = this.UsersStack.GetValue(username);
+        var text = this.UsersText.GetValue(username);
+        stack.Push(string.Join("", text));
+
+        text.AddRangeToFront(str);
+    }
+
+    public void Insert(string username, int index, string str)
+    {
+        if (!this.UsersStack.Contains(username))
+        {
+            return;
+        }
+
+        var stack = this.UsersStack.GetValue(username);
+        var text = this.UsersText.GetValue(username);
+        stack.Push(string.Join("", text));
+
+        text.InsertRange(index, str);
+    }
+
+    public void Substring(string username, int startIndex, int length)
+    {
+        if (!this.UsersStack.Contains(username))
+        {
+            return;
+        }
+
+        var stack = this.UsersStack.GetValue(username);
+        var text = this.UsersText.GetValue(username);
+        stack.Push(string.Join("", text));
+
+        BigList<char> newText = text.GetRange(startIndex, length);
+        this.UsersText.Insert(username, newText);
+    }
+
+    public void Delete(string username, int startIndex, int length)
+    {
+        if (!this.UsersStack.Contains(username))
+        {
+            return;
+        }
+
+        var stack = this.UsersStack.GetValue(username);
+        var text = this.UsersText.GetValue(username);
+        stack.Push(string.Join("", text));
+
+        this.UsersText.GetValue(username).RemoveRange(startIndex, length);
+    }
+
+    public void Clear(string username)
+    {
+        if (!this.UsersStack.Contains(username))
+        {
+            return;
+        }
+
+        var stack = this.UsersStack.GetValue(username);
+        var text = this.UsersText.GetValue(username);
+        stack.Push(string.Join("", text));
+
+        this.UsersText.GetValue(username).Clear();
+    }
+
+    public int Length(string username)
+    {
+        return this.UsersText.GetValue(username).Count;
+    }
+
+    public string Print(string username)
+    {
+        if (!this.UsersStack.Contains(username))
+        {
+            throw new InvalidOperationException();
+        }
+
+        return string.Join("", this.UsersText.GetValue(username));
+    }
+
+    public void Undo(string username)
+    {
+        if (!this.UsersStack.Contains(username) || this.UsersStack.GetValue(username).Count == 0)
+        {
+            return;
+        }
+
+        var oldText = this.UsersStack.GetValue(username).Pop();
+        BigList<char> newOldText = new BigList<char>(oldText);
+        this.UsersText.Insert(username, newOldText);
+    }
+
+    public IEnumerable<string> Users(string prefix = "")
+    {
+        return this.UsersText.GetByPrefix(prefix);
+    }
+}
