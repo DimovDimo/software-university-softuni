1f46c22014c784b290d31884251b4f48021cb128
DimovDimo <d.dimov1996@gmail.com>
2018-12-20 11:32:52 +0200

    added 02. Linked-List-CSharp-Skeleton in 04.Linear Data Structures - Stacks And Queues for Data-Structures-December-2018

---
+++ Data-Structures-December-2018/04.Linear Data Structures - Stacks And Queues/Work/02. Linked-List-CSharp-Skeleton/02. Linked-List-CSharp-Skeleton/LinkedList/LinkedList.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LinkedList<T> : IEnumerable<T>
+{
+    public class Node
+    {
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+        public Node Next { get; set; }
+    }
+
+    public Node Head { get; private set; }
+    public Node Tail { get; private set; }
+    public int Count { get; private set; }
+
+    public void AddFirst(T item)
+    {
+        Node old = this.Head;
+
+        this.Head = new Node(item);
+        this.Head.Next = old;
+
+        if (Count == 0)
+        {
+            Tail = Head;
+        }
+
+        Count++;
+    }
+
+    public void AddLast(T item)
+    {
+        Node old = this.Tail;
+        this.Tail = new Node(item);
+
+        if (Count == 0)
+        {
+            Head = Tail;
+        }
+        else
+        {
+            old.Next = this.Tail;
+        }
+
+        Count++;
+    }
+
+    public T RemoveFirst()
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        T item = this.Head.Value;
+
+        this.Head = this.Head.Next;
+
+        this.Count--;
+
+        if (Count == 0)
+        {
+            Tail = null;
+        }
+
+        return item;
+    }
+
+    public T RemoveLast()
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        T item = Tail.Value;
+
+        if(Count == 1)
+        {
+            this.Head = this.Tail = null;
+        }
+        else
+        {
+            Node parent = this.Head;
+
+            while (parent.Next != this.Tail)
+            {
+                parent = parent.Next;
+            }
+
+            parent.Next = null;
+            this.Tail = parent;
+        }
+
+        Count--;
+        return item;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        Node current = Head;
+
+        while (current != null)
+        {
+            yield return current.Value;
+            current = current.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
