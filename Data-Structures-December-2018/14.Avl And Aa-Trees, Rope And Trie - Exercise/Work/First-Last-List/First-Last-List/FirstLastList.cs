5c36bf8681d80dbedb184f253a3d3ffd46705700
DimovDimo <d.dimov1996@gmail.com>
2019-01-29 18:31:08 +0200

    added First-Last-List in 14.Avl And Aa-Trees, Rope And Trie - Exercise for Data-Structures-December-2018

---
+++ Data-Structures-December-2018/14.Avl And Aa-Trees, Rope And Trie - Exercise/Work/First-Last-List/First-Last-List/FirstLastList.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using Wintellect.PowerCollections;
+using System.Linq;
+
+public class FirstLastList<T> : IFirstLastList<T> where T : IComparable<T>
+{
+    private List<T> elements = new List<T>();
+    private OrderedBag<T> ordaredElements = new OrderedBag<T>();
+    private OrderedBag<T> reverseOrdaredElements = new OrderedBag<T>((x, y) => -x.CompareTo(y));
+
+    public int Count
+    {
+        get
+        {
+            return this.elements.Count;
+        }
+    }
+
+    public void Add(T element)
+    {
+        this.elements.Add(element);
+        this.ordaredElements.Add(element);
+        this.reverseOrdaredElements.Add(element);
+    }
+
+    public void Clear()
+    {
+        this.elements.Clear();
+        this.ordaredElements.Clear();
+        this.reverseOrdaredElements.Clear();
+    }
+
+    public IEnumerable<T> First(int count)
+    {
+        if (this.Count < count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return this.elements.Take(count);
+    }
+
+    public IEnumerable<T> Last(int count)
+    {
+        if (this.Count < count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return this.elements.Skip(Math.Max(0, this.elements.Count - count)).Reverse();
+    }
+
+    public IEnumerable<T> Max(int count)
+    {
+        if (this.Count < count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return this.reverseOrdaredElements.Take(count);
+    }
+
+    public IEnumerable<T> Min(int count)
+    {
+        if (this.Count < count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return this.ordaredElements.Take(count);
+    }
+
+    public int RemoveAll(T element)
+    {
+        foreach (var item in this.ordaredElements.Range(element, true, element, true))
+        {
+            this.elements.Remove(item);
+        }
+        int result = this.ordaredElements.RemoveAllCopies(element);
+        this.reverseOrdaredElements.RemoveAllCopies(element);
+
+        return result;
+    }
+}
