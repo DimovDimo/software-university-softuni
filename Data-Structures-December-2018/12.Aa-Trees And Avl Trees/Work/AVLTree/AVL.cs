01585d1e28ca2d2494ce301da12f462860c40fd0
DimovDimo <d.dimov1996@gmail.com>
2019-01-29 16:18:28 +0200

    added AVLTree in 12.Aa-Trees And Avl Trees for Data-Structures-December-2018

---
+++ Data-Structures-December-2018/12.Aa-Trees And Avl Trees/Work/AVLTree/AVL.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+public class AVL<T> where T : IComparable<T>
+{
+    private Node<T> root;
+
+    public Node<T> Root
+    {
+        get
+        {
+            return this.root;
+        }
+    }
+
+    public bool Contains(T item)
+    {
+        var node = this.Search(this.root, item);
+        return node != null;
+    }
+
+    public void Insert(T item)
+    {
+        this.root = this.Insert(this.root, item);
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        this.EachInOrder(this.root, action);
+    }
+
+    private Node<T> Insert(Node<T> node, T item)
+    {
+        if (node == null)
+        {
+            return new Node<T>(item);
+        }
+
+        int cmp = item.CompareTo(node.Value);
+        if (cmp < 0)
+        {
+            node.Left = this.Insert(node.Left, item);
+        }
+        else if (cmp > 0)
+        {
+            node.Right = this.Insert(node.Right, item);
+        }
+
+        node = Balance(node);
+        UpdateHeight(node);
+
+        return node;
+    }
+
+    private Node<T> Search(Node<T> node, T item)
+    {
+        if (node == null)
+        {
+            return null;
+        }
+
+        int cmp = item.CompareTo(node.Value);
+        if (cmp < 0)
+        {
+            return Search(node.Left, item);
+        }
+        else if (cmp > 0)
+        {
+            return Search(node.Right, item);
+        }
+
+        return node;
+    }
+
+    private void EachInOrder(Node<T> node, Action<T> action)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        this.EachInOrder(node.Left, action);
+        action(node.Value);
+        this.EachInOrder(node.Right, action);
+    }
+
+    private static int Height(Node<T> node)
+    {
+        if (node == null)
+        {
+            return 0;
+        }
+
+        return node.Height;
+    }
+
+    private static void UpdateHeight(Node<T> node)
+    {
+        node.Height = Math.Max(Height(node.Left), Height(node.Right)) + 1;
+    }
+
+    private Node<T> RotateLeft(Node<T> node)
+    {
+        var right = node.Right;
+        node.Right = right.Left;
+        right.Left = node;
+
+        UpdateHeight(node);
+
+        return right;
+    }
+
+    private Node<T> RotateRight(Node<T> node)
+    {
+        var left = node.Left;
+        node.Left = node.Right;
+        left.Right = node;
+
+        UpdateHeight(node);
+
+        return left;
+    }
+
+    private Node<T> Balance(Node<T> node)
+    {
+        var balance = Height(node.Left) - Height(node.Right);
+
+        if (balance > 1)
+        {
+            balance = Height(node.Left.Left) - Height(node.Left.Right);
+
+            if (balance < 0)
+            {
+                node.Left = RotateLeft(node.Left);
+            }
+
+            return RotateRight(node);
+        }
+        else if (balance < -1)
+        {
+            balance = Height(node.Right.Left) - Height(node.Right.Right);
+
+            if (balance > 0)
+            {
+                node.Right = RotateRight(node.Right);
+            }
+
+            return RotateLeft(node);
+        }
+
+        return node;
+    }
+}
