cad652ff37d30a87520b2f55fed2854138dc8106
DimovDimo <d.dimov1996@gmail.com>
2019-01-29 10:56:07 +0200

    added Sort in Heap.cs of 09.Heaps And Priority Queue for Data-Structures-December-2018

--- Data-Structures-December-2018/09.Heaps And Priority Queue/Work/BinaryHeap/Heap.cs
+++ Data-Structures-December-2018/09.Heaps And Priority Queue/Work/BinaryHeap/Heap.cs
@@ -4,6 +4,47 @@ public static class Heap<T> where T : IComparable<T>
 {
     public static void Sort(T[] arr)
     {
-        throw new NotImplementedException();
+        for (int i = arr.Length / 2; i >= 0; i--)
+        {
+            HeapifyDown(arr, i, arr.Length);
+        }
+
+        for (int i = arr.Length - 1; i > 0; i--)
+        {
+            Swap(arr, 0, i);
+            HeapifyDown(arr, 0, i);
+        }
+    }
+
+    private static void HeapifyDown(T[] arr, int current, int border)
+    {
+        while (current < border / 2)
+        {
+            int child = 2 * current + 1;
+            if (child + 1 < border && IsGreater(arr, child + 1, child))
+            {
+                child++;
+            }
+
+            if (IsGreater(arr, current, child))
+            {
+                break;
+            }
+
+            Swap(arr, child, current);
+            current = child;
+        }
+    }
+
+    private static bool IsGreater(T[] arr, int index, int parent)
+    {
+        return arr[index].CompareTo(arr[parent]) > 0;
+    }
+
+    private static void Swap(T[] arr, int index, int parent)
+    {
+        var temp = arr[parent];
+        arr[parent] = arr[index];
+        arr[index] = temp;
     }
 }
