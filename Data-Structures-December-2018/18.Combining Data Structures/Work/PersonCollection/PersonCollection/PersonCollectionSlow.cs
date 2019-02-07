bcae9cfd13cee45dfe8212f5b55a2a795f258710
DimovDimo <d.dimov1996@gmail.com>
2019-02-07 21:24:17 +0200

    added PersonCollectionSlow in 18.Combining Data Structures for Data-Structures-December-2018

---
+++ Data-Structures-December-2018/18.Combining Data Structures/Work/PersonCollection/PersonCollection/PersonCollectionSlow.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PersonCollectionSlow : IPersonCollection
+{
+    private List<Person> persons = new List<Person>();
+
+    public bool AddPerson(string email, string name, int age, string town)
+    {
+        if (this.FindPerson(email) != null)
+        {
+            return false;
+        }
+
+        var person = new Person()
+        {
+            Email = email,
+            Name = name,
+            Age = age,
+            Town = town
+        };
+
+        this.persons.Add(person);
+
+        return true;
+    }
+
+    public int Count
+    {
+        get
+        {
+            return this.persons.Count;
+        }
+    }
+
+    public Person FindPerson(string email)
+    {
+        foreach (var person in this.persons)
+        {
+            if (person.Email == email)
+            {
+                return person;
+            }
+        }
+
+        return null;
+    }
+
+    public bool DeletePerson(string email)
+    {
+        var person = this.FindPerson(email);
+
+        return this.persons.Remove(person);
+    }
+
+    public IEnumerable<Person> FindPersons(string emailDomain)
+    {
+        return this.persons
+            .Where(p => p.Email.EndsWith("@" + emailDomain))
+            .OrderBy(p => p.Email);
+    }
+
+    public IEnumerable<Person> FindPersons(string name, string town)
+    {
+        return this.persons
+            .Where(p => p.Name == name && p.Town == town)
+            .OrderBy(p => p.Email);
+    }
+
+    public IEnumerable<Person> FindPersons(int startAge, int endAge)
+    {
+        return this.persons
+            .Where(p => p.Age >= startAge && p.Age <= endAge)
+            .OrderBy(p => p.Age)
+            .ThenBy(p => p.Email);
+    }
+
+    public IEnumerable<Person> FindPersons(
+        int startAge, int endAge, string town)
+    {
+        return this.persons
+            .Where(p => p.Town == town)
+            .Where(p => startAge <= p.Age && p.Age <= endAge)
+            .OrderBy(p => p.Age)
+            .ThenBy(p => p.Email);
+    }
+}
