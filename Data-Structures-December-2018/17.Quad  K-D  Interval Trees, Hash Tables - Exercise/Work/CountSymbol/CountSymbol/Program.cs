302ced441bb2101d33c9e2fa1e32e188229d8953
DimovDimo <d.dimov1996@gmail.com>
2019-02-10 15:24:53 +0200

    added CountSymbol & Phonebook in 17.Quad  K-D  Interval Trees, Hash Tables - Exercise for Data-Structures-December-2018

---
+++ Data-Structures-December-2018/17.Quad  K-D  Interval Trees, Hash Tables - Exercise/Work/CountSymbol/CountSymbol/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountSymbol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<char, int>();
+            var input = Console.ReadLine();
+            foreach (char charElement in input)
+            {
+                if (!dictionary.ContainsKey(charElement))
+                {
+                    dictionary[charElement] = 0;
+                }
+
+                dictionary[charElement]++;
+            }
+
+            foreach (var keyValuePair in dictionary
+                .OrderBy(x => x.Key))
+            {
+                Console.WriteLine(keyValuePair.Key + ": " 
+                    + keyValuePair.Value + " time/s");
+            }
+        }
+    }
+}
---
+++ Data-Structures-December-2018/17.Quad  K-D  Interval Trees, Hash Tables - Exercise/Work/Phonebook/Phonebook/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            string keyValue = Console.ReadLine();
+            while (keyValue != "search")
+            {
+                string[] elements = keyValue.Split('-');
+                string person = elements[0];
+                string number = elements[1];
+                dictionary[person] = number;
+                keyValue = Console.ReadLine();
+            }
+
+            string search = Console.ReadLine();
+            while (search != "end")
+            {
+                if (dictionary.TryGetValue(search, out string value))
+                {
+                    Console.WriteLine(search + " -> " + value);
+                }
+                else
+                {
+                    Console.WriteLine($"Contact {search} does not exist.");
+                }
+
+                search = Console.ReadLine();
+            }
+        }
+    }
+}
