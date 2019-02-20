7aa55e6189fddb458a4278dcbe51363682dea850
DimovDimo <d.dimov1996@gmail.com>
2019-02-20 19:40:23 +0200

    added Data Structures Exam Retake - 20 May 2018 - C# for Data-Structures-December-2018

---
+++ Data-Structures-December-2018/Data Structures Exam Retake - 20 May 2018 - C#/01. Scheduler/Work/01. Scheduler - Correctness_01. Scheduler - Skeleton/ThreadExecutor/ThreadExecutor.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+/// <summary>
+/// The ThreadExecutor is the concrete implementation of the IScheduler.
+/// You can send any class to the judge system as long as it implements
+/// the IScheduler interface. The Tests do not contain any <e>Reflection</e>!
+/// </summary>
+public class ThreadExecutor : IScheduler
+{
+    Dictionary<int, Task> byId = 
+        new Dictionary<int, Task>();
+    int sumCyrcles = 0;
+ 
+    int IScheduler.Count => byId.Count();
+
+    public ThreadExecutor()
+    {
+
+    }
+
+    public int Count()
+    {
+        return byId.Count();
+    }
+
+
+    public void ChangePriority(int id, Priority newPriority)
+    {
+        if (!byId.ContainsKey(id))
+        {
+            throw new ArgumentException();
+        }
+
+        byId[id].TaskPriority = newPriority;
+    }
+
+    public bool Contains(Task task)
+    {
+        return byId.ContainsKey(task.Id);
+    }
+
+    public int Cycle(int cycles)
+    {
+        if (byId.Count() == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        int startCount = byId.Count();
+
+        Dictionary<int, Task> newDic = new Dictionary<int, Task>();
+        
+        sumCyrcles += cycles;
+        foreach (var item in byId.Values)
+        {
+            if(item.Consumption - sumCyrcles > 0)
+            {
+                newDic.Add(item.Id, item);
+            }            
+        }
+        
+        byId = newDic;
+
+        //byId.Where(x => x.Value.Consumption - sumCyrcles > 0);
+
+        return startCount - byId.Count();
+
+    }
+
+    public void Execute(Task task)
+    {
+        if (byId.ContainsKey(task.Id))
+        {
+            throw new ArgumentException();
+        }
+
+        byId.Add(task.Id, task);
+    }
+
+    public IEnumerable<Task> GetByConsumptionRange(int lo, int hi, bool inclusive)
+    {
+        if (inclusive)
+        {
+            return byId.Values
+                .Where(x => lo <= x.Consumption - sumCyrcles
+                    && x.Consumption - sumCyrcles <= hi)
+                .OrderBy(x => x.Consumption)
+                .ThenByDescending(x => x.TaskPriority);
+        }
+        else
+        {
+            return byId.Values
+                .Where(x => lo < x.Consumption - sumCyrcles
+                    && x.Consumption - sumCyrcles < hi)
+                .OrderBy(x => x.Consumption)
+                .ThenByDescending(x => x.TaskPriority);
+        }
+        
+    }
+
+    public Task GetById(int id)
+    {
+        if (!byId.ContainsKey(id))
+        {
+            throw new ArgumentException();
+        }
+
+        return byId[id];
+    }
+
+    public Task GetByIndex(int index)
+    {
+        if (index < 0 
+            || byId.Count() - 1 < index)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return byId.Values.ElementAt(index);
+    }
+
+    public IEnumerable<Task> GetByPriority(Priority type)
+    {
+        return byId.Values
+            .Where(x => x.TaskPriority == type)
+            .OrderByDescending(x => x.Id);
+    }
+
+    public IEnumerable<Task> GetByPriorityAndMinimumConsumption(Priority priority, int lo)
+    {
+        var result = byId.Values
+                .Where(x => lo <= x.Consumption
+                    && x.TaskPriority == priority)
+                .OrderByDescending(x => x.Id);
+
+        if (result.Count() == 0)
+        {
+            return new List<Task>();
+        }
+
+        return result;
+    }
+
+
+    public IEnumerator<Task> GetEnumerator()
+    {
+        foreach (var item in byId.Values)
+        {
+            yield return item;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
---
+++ Data-Structures-December-2018/Data Structures Exam Retake - 20 May 2018 - C#/02. RoyaleArena/Work/02. RoyaleArena - Correctness_02. RoyaleArena - Skeleton/RoyaleArena/RoyaleArena/RoyaleArena.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+public class RoyaleArena : IArena
+{
+    Dictionary<int, Battlecard> byId =
+        new Dictionary<int, Battlecard>();
+    Dictionary<string, Battlecard> byName = 
+        new Dictionary<string, Battlecard>();
+
+    public int Count => byId.Count();
+
+    public void Add(Battlecard card)
+    {
+        byId.Add(card.Id, card);
+    }
+
+    public void ChangeCardType(int id, CardType type)
+    {
+        if (!byId.ContainsKey(id))
+        {
+            throw new ArgumentException();
+        }
+
+        byId[id].Type = type;
+    }
+
+    public bool Contains(Battlecard card)
+    {
+        return byId.ContainsKey(card.Id);
+    }
+
+    public IEnumerable<Battlecard> FindFirstLeastSwag(int n)
+    {
+        if (byId.Count() < n)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return byId.Values
+            .OrderBy(x => x.Swag)
+            .ThenBy(x => x.Id)
+            .Take(n);
+    }
+
+    public IEnumerable<Battlecard> GetAllByNameAndSwag()
+    {
+        byName.Clear();
+
+        foreach(var item in byId.Values)
+        {
+            if (byName.ContainsKey(item.Name))
+            {
+                if(byName[item.Name].Swag < item.Swag)
+                {
+                    byName[item.Name] = item;
+                }
+            }
+            else
+            {
+                byName.Add(item.Name, item);
+            }
+        }
+
+        if (byName.Count() == 0)
+        {
+            return new List<Battlecard>();
+        }
+
+        return byName.Values;
+    }
+
+    public IEnumerable<Battlecard> GetAllInSwagRange(double lo, double hi)
+    {
+        return byId.Values
+            .Where(x => lo <= x.Swag
+                       && x.Swag <= hi)
+            .OrderBy(x => x.Swag);
+    }
+
+    public IEnumerable<Battlecard> GetByCardType(CardType type)
+    {
+        IOrderedEnumerable<Battlecard> results = byId.Values
+            .Where(x => x.Type == type)
+            .OrderByDescending(x => x.Damage)
+            .ThenBy(x => x.Id);
+
+        if (results.Count() == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return results;
+    }
+
+    public IEnumerable<Battlecard> GetByCardTypeAndMaximumDamage(CardType type, double damage)
+    {
+        IOrderedEnumerable<Battlecard> results = byId.Values
+            .Where(x => x.Type == type
+                     && x.Damage <= damage)
+            .OrderByDescending(x => x.Damage)
+            .ThenBy(x => x.Id);
+
+        if (results.Count() == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return results;
+    }
+
+    public Battlecard GetById(int id)
+    {
+        if (!byId.ContainsKey(id))
+        {
+            throw new InvalidOperationException();
+        }
+
+        return byId[id];
+    }
+
+    public IEnumerable<Battlecard> GetByNameAndSwagRange(string name, double lo, double hi)
+    {
+        IOrderedEnumerable<Battlecard> results = byId.Values
+            .Where(x => x.Name == name
+                      && lo <= x.Swag
+                      && x.Swag < hi)
+            .OrderByDescending(x => x.Swag)
+            .ThenBy(x => x.Id);
+
+        if (results.Count() == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return results;
+    }
+
+    public IEnumerable<Battlecard> GetByNameOrderedBySwagDescending(string name)
+    {
+        IOrderedEnumerable<Battlecard> results = byId.Values
+            .Where(x => x.Name == name)
+            .OrderByDescending(x => x.Swag)
+            .ThenBy(x => x.Id);
+
+        if (results.Count() == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return results;
+    }
+
+    public IEnumerable<Battlecard> GetByTypeAndDamageRangeOrderedByDamageThenById(CardType type, int lo, int hi)
+    {
+        IOrderedEnumerable<Battlecard> results = byId.Values
+            .Where(x => x.Type == type
+                     && lo < x.Damage
+                     && x.Damage <= hi)
+            .OrderByDescending(x => x.Damage)
+            .ThenBy(x => x.Id);
+
+        if (results.Count() == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return results;
+    }
+
+    public IEnumerator<Battlecard> GetEnumerator()
+    {
+        foreach (var item in byId.Values)
+        {
+            yield return item;
+        }
+    }
+
+    public void RemoveById(int id)
+    {
+        if (!byId.ContainsKey(id))
+        {
+            throw new InvalidOperationException();
+        }
+        
+        byId.Remove(id);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
