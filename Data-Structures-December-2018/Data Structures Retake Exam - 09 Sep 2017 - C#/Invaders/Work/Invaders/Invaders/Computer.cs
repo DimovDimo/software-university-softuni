85c4ab3fc10d7312b2e66488f1e83c929f48eb75
DimovDimo <d.dimov1996@gmail.com>
2019-02-21 21:17:49 +0200

    added Invaders & Judge in Data Structures Retake Exam - 09 Sep 2017 - C# for Data-Structures-December-2018

---
+++ Data-Structures-December-2018/Data Structures Retake Exam - 09 Sep 2017 - C#/Invaders/Work/Invaders/Invaders/Computer.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Wintellect.PowerCollections;
+using System.Linq;
+
+public class Computer : IComputer
+{
+    private LinkedList<Invader> byInsertion = new LinkedList<Invader>();
+    private Dictionary<int, List<LinkedListNode<Invader>>> byDistance =
+        new Dictionary<int, List<LinkedListNode<Invader>>>();
+
+    int energy;
+    int steps = 0;
+
+    public Computer(int energy)
+    {
+        if(energy < 0)
+        {
+            throw new ArgumentException();
+        }
+
+        this.energy = energy;
+    }
+
+    public int Energy
+    {
+        get
+        {
+            if (energy < 0) return 0;
+            return energy;
+        }
+    }
+
+    public void Skip(int turns)
+    {
+        steps += turns;
+
+        this.byDistance = this.byDistance.Where((x) =>
+        {
+            int remDistance = x.Key - this.steps;
+
+            if (remDistance <= 0)
+            {
+                energy -= x.Value.Sum(y => y.Value.Damage);
+                x.Value.ForEach(y => this.byInsertion.Remove(y));
+            }
+
+            return remDistance > 0;
+        }).ToDictionary(x => x.Key, y => y.Value);
+    }
+
+    public void AddInvader(Invader invader)
+    {
+        LinkedListNode<Invader> node =
+            new LinkedListNode<Invader>(invader);
+        if (!this.byDistance.ContainsKey(invader.Distance))
+        {
+            this.byDistance.Add(invader.Distance, new List<LinkedListNode<Invader>>());
+        }
+        this.byInsertion.AddLast(node);
+        this.byDistance[invader.Distance].Add(node);
+    }
+
+    public void DestroyHighestPriorityTargets(int count)
+    {
+        foreach (var linkedListNode in this.byDistance
+            .SelectMany(x => x.Value)
+            .OrderBy(x => x.Value)
+            .Take(count))
+        {
+            this.byInsertion.Remove(linkedListNode);
+        }
+        var newDict = this.byDistance
+            .SelectMany(x => x.Value)
+            .OrderBy(x => x.Value)
+            .Skip(count);
+
+        this.byDistance = new Dictionary<int, List<LinkedListNode<Invader>>>();
+        foreach (var item in newDict)
+        {
+            if (!this.byDistance.ContainsKey(item.Value.Distance))
+            {
+                this.byDistance.Add(item.Value.Distance, new List<LinkedListNode<Invader>>());
+            }
+            this.byDistance[item.Value.Distance].Add(item);
+        }
+
+        //int counter = 0;
+        //OrderedBag<Invader> forRemove = new OrderedBag<Invader>();
+        //firstByDistanceThenByDamage
+        //    .ForEach(x =>
+        //    {
+        //        counter++;
+        //        if (counter <= count)
+        //        {
+        //            forRemove.Add(x);
+        //        }
+        //        else
+        //        {
+        //            return;
+        //        }
+        //    });
+        //
+        //firstByDistanceThenByDamage.RemoveMany(forRemove);
+    }
+
+    public void DestroyTargetsInRadius(int radius)
+    {
+        this.byDistance = this.byDistance
+            .Where(x =>
+            {
+                bool result = x.Key - this.steps > radius;
+
+                if (!result)
+                {
+                    x.Value.ForEach(y => this.byInsertion.Remove(y));
+                }
+                return result;
+            })
+            .ToDictionary(x => x.Key, y => y.Value);
+    }
+
+    public IEnumerable<Invader> Invaders()
+    {
+        return byInsertion;
+    }
+}
---
+++ Data-Structures-December-2018/Data Structures Retake Exam - 09 Sep 2017 - C#/Judge/Work/SimpleJudge/Judge.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Wintellect.PowerCollections;
+using System.Linq;
+
+public class Judge : IJudge
+{
+    HashSet<int> byUserId = new HashSet<int>();
+    HashSet<int> byContestId = new HashSet<int>();
+    Dictionary<int, Submission> byId =
+        new Dictionary<int, Submission>();
+
+    public void AddContest(int contestId)
+    {
+        byContestId.Add(contestId);
+    }
+
+    public void AddSubmission(Submission submission)
+    {
+        if(!byUserId.Contains(submission.UserId) ||
+            !byContestId.Contains(submission.ContestId))
+        {
+            throw new InvalidOperationException();
+        }
+
+        if (this.byId.ContainsKey(submission.Id))
+        {
+            return;
+        }
+
+        byId.Add(submission.Id, submission);
+    }
+
+    public void AddUser(int userId)
+    {
+        byUserId.Add(userId);
+    }
+
+    public void DeleteSubmission(int submissionId)
+    {
+        if (!byId.ContainsKey(submissionId))
+        {
+            throw new InvalidOperationException();
+        }
+        
+        byId.Remove(submissionId);
+       
+    }
+
+    public IEnumerable<Submission> GetSubmissions()
+    {
+        return byId.Values
+            .OrderBy(x => x.Id);
+    }
+
+    public IEnumerable<int> GetUsers()
+    {
+        return byUserId.OrderBy(x => x);
+    }
+
+    public IEnumerable<int> GetContests()
+    {
+        return byContestId.OrderBy(x => x);
+    }
+
+    public IEnumerable<Submission> SubmissionsWithPointsInRangeBySubmissionType(int minPoints, int maxPoints, SubmissionType submissionType)
+    {
+        return byId.Values
+            .Where(x => x.Type == submissionType
+                   && minPoints <= x.Points
+                   && x.Points <= maxPoints);
+    }
+
+    public IEnumerable<int> ContestsByUserIdOrderedByPointsDescThenBySubmissionId(int userId)
+    {
+        return byId.Values
+            .Where(x => x.UserId == userId)
+            .OrderByDescending(x => x.Points)
+            .ThenBy(x => x.Id)
+            .Select(x => x.ContestId)
+            .Distinct();
+    }
+
+    public IEnumerable<Submission> SubmissionsInContestIdByUserIdWithPoints(int points, int contestId, int userId)
+    {
+        if (!byUserId.Contains(userId) ||
+            !byContestId.Contains(contestId))
+        {
+            throw new InvalidOperationException();
+        }
+
+        var result = byId.Values
+            .Where(x => x.Points == points
+                   && x.ContestId == contestId
+                   && x.UserId == userId);
+
+        if(result.Count() == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return result;
+    }
+
+    public IEnumerable<int> ContestsBySubmissionType(SubmissionType submissionType)
+    {
+        return byId.Values
+            .Where(x => x.Type == submissionType)
+            .Select(x => x.ContestId)
+            .Distinct();
+    }
+}
