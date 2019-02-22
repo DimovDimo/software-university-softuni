5ea673a2900982ac162c8a7413316109f5e5c6bf
DimovDimo <d.dimov1996@gmail.com>
2019-02-22 19:27:52 +0200

    added Again on 22.2.2019 in Data Structures Exam - 11 Mar 2018 - C# for Data-Structures-December-2018

---
+++ Data-Structures-December-2018/Data Structures Exam - 11 Mar 2018 - C#/Again on 22.2.2019/Chainblock/Work/Chainblock/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Transaction : IComparable<Transaction>
+{
+    public int Id { get; set; }
+    public TransactionStatus Status { get; set; }
+    public string From { get; set; }
+    public string To { get; set; }
+    public double Amount { get; set; }
+
+    public Transaction(int id, TransactionStatus st, string from, string to, double amount)
+    {
+        this.Id = id;
+        this.Status = st;
+        this.From = from;
+        this.To = to;
+        this.Amount = amount;
+    }
+
+    public int CompareTo(Transaction other)
+    {
+        //TODO
+        return 0;
+    }
+}
---
+++ Data-Structures-December-2018/Data Structures Exam - 11 Mar 2018 - C#/Again on 22.2.2019/INstock/Work/PeshoAndCo/Instock.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+//using Wintellect.PowerCollections;
+using System.Linq;
+
+public class Instock : IProductStock
+{
+    Dictionary<string, Product> byLabel =
+        new Dictionary<string, Product>();
+
+    public int Count => byLabel.Count();
+
+    public void Add(Product product)
+    {
+        if (byLabel.ContainsKey(product.Label))
+        {
+        
+        }
+
+        byLabel.Add(product.Label, product);
+    }
+
+    public void ChangeQuantity(string product, int quantity)
+    {
+        if (!byLabel.ContainsKey(product))
+        {
+            throw new ArgumentException();
+        }
+
+        byLabel[product].Quantity = quantity;
+
+        //Product item = byLabel[product];
+        //item.Quantity = quantity;
+        //byLabel.Remove(product);
+        //byLabel.Add(item.Label, item);
+        
+        //var newDic = byLabel.Values
+        //    .Where(x => x.Label != product)
+        //    .ToDictionary(x => x.Label, y => y);
+        //newDic.Add(item.Label, item);
+        //byLabel = newDic;
+    }
+
+    public bool Contains(Product product)
+    {
+        return byLabel.ContainsKey(product.Label);
+    }
+
+    public Product Find(int index)
+    {
+        if (index < 0 || byLabel.Count() - 1 < index)
+        {
+            throw new IndexOutOfRangeException();
+        }
+
+        return byLabel.Values.ToList()[index];
+    }
+
+    public IEnumerable<Product> FindAllByPrice(double price)
+    {
+        return byLabel.Values
+            .Where(x => x.Price == price);
+    }
+
+    public IEnumerable<Product> FindAllByQuantity(int quantity)
+    {
+        return byLabel.Values
+            .Where(x => x.Quantity == quantity);
+    }
+
+    public IEnumerable<Product> FindAllInRange(double lo, double hi)
+    {
+        return byLabel.Values
+            .Where(x => lo < x.Price && x.Price <= hi)
+            .OrderByDescending(x => x.Price);
+    }
+
+    public Product FindByLabel(string label)
+    {
+        if (!byLabel.ContainsKey(label))
+        {
+            throw new ArgumentException();
+        }
+
+        return byLabel[label];
+    }
+
+    public IEnumerable<Product> FindFirstByAlphabeticalOrder(int count)
+    {
+        if (count == 0)
+        {
+            return new List<Product>();
+        }
+
+        if (count < 0 || byLabel.Count() < count)
+        {
+            throw new ArgumentException();
+        }        
+
+        return byLabel.Values
+            .OrderBy(x => x.Label)
+            .Take(count);
+    }
+
+    public IEnumerable<Product> FindFirstMostExpensiveProducts(int count)
+    {
+        if (byLabel.Count() < count)
+        {
+            throw new ArgumentException();
+        }
+
+        return byLabel.Values
+            .OrderByDescending(x => x.Price)
+            .Take(count);
+
+        //var result = byLabel.Values
+        //    .OrderByDescending(x => x.Price)
+        //    .Take(count);
+        //
+        //if (result.Count() < count)
+        //{
+        //    throw new ArgumentException();
+        //}
+        //
+        //return result;
+    }
+
+    public IEnumerator<Product> GetEnumerator()
+    {
+        return byLabel.Values.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
