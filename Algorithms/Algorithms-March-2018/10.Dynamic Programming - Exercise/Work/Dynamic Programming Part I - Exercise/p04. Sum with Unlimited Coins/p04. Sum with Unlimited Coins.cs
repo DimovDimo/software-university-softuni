7883007d6f674332d69170bd6f613ef4b4de26ad
DimovDimo <d.dimov1996@gmail.com>
2019-03-18 16:32:58 +0200

    added Part I in 10.Dynamic Programming - Exercise for Algorithms-March-2018

---
+++ Algorithms/Algorithms-March-2018/10.Dynamic Programming - Exercise/Work/Dynamic Programming Part I - Exercise/p04. Sum with Unlimited Coins/p04. Sum with Unlimited Coins.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p04.Sum_with_Unlimited_Coins
+{
+    class Program
+    {
+        private static int countComb;
+        private static int sum;
+        private static int[] coins;
+        private static List<int> getCoins = new List<int>();
+        private static StringBuilder comb = new StringBuilder();
+
+        static void Main(string[] args)
+        {
+            coins = Console.ReadLine().Split().Select(int.Parse).Reverse().ToArray();
+            sum = int.Parse(Console.ReadLine());
+            Combinations(0, sum);
+            Result();
+        }
+
+        private static void Result()
+        {
+            Console.WriteLine(countComb);
+            //Console.WriteLine($"The {countComb} combinations are:");
+            //Console.WriteLine(comb.ToString());
+        }
+
+        private static void Combinations(int index, int different)
+        {
+            if (index >= coins.Length)
+            {
+                return;
+            }
+            else if (different == 0)
+            {
+                MakePrint();
+            }
+            else if (different > 0)
+            {
+                int currentNum = coins[index];
+                if (different - currentNum >= 0)
+                {
+                    //getCoins.Add(currentNum);
+                    Combinations(index, different - currentNum);
+                }
+                
+                Combinations(index + 1, different);
+            }
+        }
+
+        private static void MakePrint()
+        {
+            countComb++;
+            //comb.AppendLine($"{sum} = " + string.Join(" + ", getCoins));
+            //getCoins.Clear();
+        }
+    }
+}
---
+++ Algorithms/Algorithms-March-2018/10.Dynamic Programming - Exercise/Work/Dynamic Programming Part I - Exercise/p05. Sum with Limited Coins/p05. Sum with Limited Coins.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p05.Sum_with_Limited_Coins
+{
+    class Program
+    {
+        private static int sum;
+        private static int[] coins;
+        private static HashSet<string> distinctComb = new HashSet<string>();
+        private static List<int> combs = new List<int>();
+
+        static void Main(string[] args)
+        {
+            coins = Console.ReadLine().Split().Select(int.Parse).Reverse().ToArray();
+            sum = int.Parse(Console.ReadLine());
+            Combinations(0, sum);
+            Result();
+        }
+
+        private static void Result()
+        {
+            Console.WriteLine(distinctComb.Count());
+        }
+
+        private static void Combinations(int index, int different)
+        {
+            for (int i = index; i < coins.Length; i++)
+            {
+                int currentNum = coins[i];
+                if (different - currentNum == 0)
+                {
+                    combs.Add(currentNum);
+                    distinctComb.Add(string.Join(" ", combs));
+                    Console.WriteLine(string.Join(" ", combs));
+                    //combs.Clear();
+                }
+            
+                Combinations(i + 1, different - currentNum);
+            }
+
+            //for (int i = index; i < coins.Length; i++)
+            //{
+            //    int currentNum = coins[i];
+            //    Console.Write(currentNum + " ");
+            //
+            //    Combinations(i + 1, different);
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
