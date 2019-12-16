e69d9de0064093ba2f2dbf10f5ce134d053b65ed
DimovDimo <d.dimov1996@gmail.com>
2019-12-16 16:30:09 +0200

    removed files

--- JS-Web/ExpressJS-Fundamentals-May-2018/05.Exercise Web Server And Development Tools/Work/Web Server/handlers/errorHandler.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-    res.writeHead(404, {
-        'Content-Type': 'text/plain'
-    });
-    res.end('Bad Request');
-}
--- JS-Web/ExpressJS-Fundamentals-May-2018/05.Exercise Web Server And Development Tools/Work/Web Server/handlers/homeHandler.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-    if (req.urlData.pathname === '/' && req.method === 'GET') {
-        res.view('views/home.html');
-    } else {
-        return true;
-    }
-}
